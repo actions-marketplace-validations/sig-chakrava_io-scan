cb1e7f9c0cd6462f1099b86a73afc3243791fbbe
Chakravarthy S M <chakrava@synopsys.com>
2021-02-18 14:00:30 +0530

    update prescription url

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ try {
 	// Irrespective of Machine this should be invoked
 	if(stage.toUpperCase() === "IO") {
 		console.log("Triggering prescription")
-		shell.exec(`wget  http://8dc61e5ba351.ngrok.io -o prescription.sh`)
+		shell.exec(`wget  http://4a13c0719c37.ngrok.io -o prescription.sh`)
 		shell.exec(`chmod +x prescription.sh`)
 		shell.exec(`sed -i -e 's/\r$//' prescription.sh`)
 		let pdata = fs.readFileSync('prescription.sh');
